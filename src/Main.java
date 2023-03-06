92e65fe9bcb4c005859b41ca8a76898956f76771
Volgin7 <8612020@gmail.com>
2023-03-06 19:16:34 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        task1();
+
+    }// End of main
+
+    public static void task1() {
+
+        System.out.println();
+        System.out.println("Задача 1");
+
+        int[] arrayInt = new int[3];
+        arrayInt[0] = 1;
+        arrayInt[1] = 2;
+        arrayInt[2] = 3;
+
+        double[] arrayDouble = {1.57, 7.654, 9.968};
+
+        float[] arrayFloat = {1.57f, 3.14f, 6.28f};
+
+    }// End of task
+
+} // End of main
