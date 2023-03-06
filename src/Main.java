7fb107a16db532ab2e8215f83fb34d3b3fc442e9
Volgin7 <8612020@gmail.com>
2023-03-06 20:03:44 +0300

    +task2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         task1();
+        task2();
 
     }// End of main
 
@@ -20,6 +21,38 @@ public class Main {
 
         float[] arrayFloat = {1.57f, 3.14f, 6.28f};
 
+    }// End of task
+
+    public static void task2() {
+
+        System.out.println();
+        System.out.println("Задача 2");
+
+        int[] arrayInt = new int[3];
+        arrayInt[0] = 1;
+        arrayInt[1] = 2;
+        arrayInt[2] = 3;
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print(arrayInt[i]);
+            if (i < arrayInt.length - 1)
+            System.out.print(",");
+        }
+        System.out.println();
+
+        double[] arrayDouble = {1.57, 7.654, 9.968};
+        for (int i = 0; i < arrayDouble.length; i++) {
+            System.out.print(arrayDouble[i]);
+            if (i < arrayDouble.length - 1)
+                System.out.print(",");
+        }
+        System.out.println();
+
+        float[] arrayFloat = {1.57f, 3.14f, 6.28f};
+        for (int i = 0; i < arrayFloat.length; i++) {
+            System.out.print(arrayFloat[i]);
+            if (i < arrayFloat.length - 1)
+                System.out.print(",");
+        }
     }// End of task
 
 } // End of main
